7956eb0ea890fb53977a80c51ca82d0c77d34a15
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-25 11:00:57 +0530

    databinding

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-employee.component.css']
 })
 export class ViewEmployeeComponent {
-
+  data=[
+    {
+      "name":"Sruthy",
+      "designation":"Software Engineer Trainee",
+      "salary":25000,
+      "company":"NEST"
+    },
+    {
+      "name":"Tania",
+      "designation":"Software Engineer Trainee",
+      "salary":25000,
+      "company":"LULU"
+    },
+    {
+      "name":"Ninu",
+      "designation":"Software Engineer Trainee",
+      "salary":25000,
+      "company":"Muziris"
+    },
+    {
+      "name":"Ashmi",
+      "designation":"Software Engineer Trainee",
+      "salary":25000,
+      "company":"Big Formula"
+    },
+    {
+      "name":"Abitta",
+      "designation":"Software Engineer Trainee",
+      "salary":25000,
+      "company":"Suyati"
+    }
+  ]
 }
